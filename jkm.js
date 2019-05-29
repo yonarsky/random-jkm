02ab18c18915b1895a29a4309c47d6507eb0fa0f
Adrian <lxge99@gmail.com>
2019-05-29 18:45:30 +0200

    na wielowym tab

--- jkm.js
+++ jkm.js
@@ -18,10 +18,22 @@ const losowanie = function() {
 const button = document.querySelector("button");
 button.addEventListener("click", losowanie);*/
 
-const kol1 = ['Ja chcę powiedzieć jedną rzecz:', 'Trzeba powiedzieć jasno:', 'Jak powiedział wybitny krakowianin Stanisław Lem,'];
-const kol2 = ['przedstawiciele czerwonej hołoty', 'ci wszyscy (tfu) geje', 'funkconariusze reżimowej telewizji'];
-const kol3 = ['zupełnie bezkarnie', 'całkowicie bezczelnie', 'o poglądach na lewo od komunizmu'];
-const kol4 = ['nawolują do podniesienia podatków', 'próbują wyrzucić kierowców z miast', 'próbują skłócić Polskę z Rosją'];
-const kol5 = ['bo dzięki temu mogą kraść', 'bo dostają za to pieniądze', 'bo tak się uczy w państwowej szkole'];
-const kol6 = ['przez kolejne kadencje', 'o czym się nie mówi', 'i właśnie dlatego Europa umiera'];
-
+const tab1 = [['Ja chcę powiedzieć jedną rzecz:', 'Trzeba powiedzieć jasno:', 'Jak powiedział wybitny krakowianin Stanisław Lem,'],
+['przedstawiciele czerwonej hołoty', 'ci wszyscy (tfu) geje', 'funkconariusze reżimowej telewizji'],
+['zupełnie bezkarnie', 'całkowicie bezczelnie', 'o poglądach na lewo od komunizmu'],
+['nawolują do podniesienia podatków', 'próbują wyrzucić kierowców z miast', 'próbują skłócić Polskę z Rosją'],
+['bo dzięki temu mogą kraść', 'bo dostają za to pieniądze', 'bo tak się uczy w państwowej szkole'],
+['przez kolejne kadencje', 'o czym się nie mówi', 'i właśnie dlatego Europa umiera']];
+const losowanie = function() {
+     const wynikLos = Math.floor(Math.random() * 49 + 1);
+     const div1 = document.createElement("div1");
+     for (let i = 0; i < 6; i++) {
+          div1.textContent = tab1[i][wynikLos];
+          }
+      }
+     div1.className = "kula";
+     document.getElementById("blok").appendChild(div1);
+     result.push(wynikLos);
+}
+const button = document.querySelector("button");
+button.addEventListener("click", losowanie);
